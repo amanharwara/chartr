cd300c101605f3b568df845b4e0e83c8cc2b5eab
Aman <amanharwara@protonmail.com>
2020-10-11 23:04:27 +0530

    Make album collage item deletable using keyboard

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,26 @@
 <script>
   import MainView from "./layouts/MainView.svelte";
   import SettingsView from "./layouts/SettingsView.svelte";
-  import { settingsVisible } from "./store";
+  import { settingsVisible, currentChartList, currentChartId } from "./store";
 </script>
 
 <style lang="scss">
 </style>
+
+<svelte:body
+  on:keydown={(e) => {
+    let activeElement = document.activeElement.closest('.column');
+    if (activeElement) {
+      let row_index = activeElement.dataset.row_index;
+      let column_index = activeElement.dataset.column_index;
+
+      if (e.key === 'Delete') {
+        let temp_list = $currentChartList.find((chart) => chart.id === $currentChartId).albumCollageList;
+        temp_list[row_index][column_index] = undefined;
+        $currentChartList[$currentChartList.findIndex((chart) => chart.id === $currentChartId)].albumCollageList = temp_list;
+      }
+    }
+  }} />
 
 {#if $settingsVisible}
   <SettingsView />
