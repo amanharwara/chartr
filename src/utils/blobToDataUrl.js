2f90a3dec5199ef4074d9c7deb1cee2afb3380fb
Aman Harwara <amanharwara@protonmail.com>
2022-02-02 12:02:29 +0530

    refactor: initial commit for solid rewrite

--- src/utils/blobToDataUrl.js
+++
@@ -1,9 +0,0 @@
-const blobToDataUrl = (blob, callback) => {
-  let a = new FileReader();
-  a.onload = function (e) {
-    callback(e.target.result);
-  };
-  a.readAsDataURL(blob);
-};
-
-export default blobToDataUrl;
